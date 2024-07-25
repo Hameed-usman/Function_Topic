ea2ddec3e419507fc34c63a138eeaa78a8d0e314
Hameed-usman <usman.hameed1145@gmail.com>
2024-07-26 01:25:38 +0500

    JavaScript Functions

---
+++ script.js
@@ -0,0 +1,49 @@
+//                        "TASK 01"
+// Write a JavaScript function that takes two numbers as arguments and returns their sum.
+// function addFunction(num1, num2) {
+//     return num1 + num2;
+// }
+
+// console.log(addFunction(2, 4)); 
+// console.log (addFunction(11,11));
+// console.log(addFunction(10,20));
+
+ 
+//                  TASK 02
+// Write a JavaScript function that takes a string as an argument and returns the string in uppercase.
+// function toUpper (str){
+//     return str.toUpperCase ();
+// }                
+// console.log(toUpper("usman"));
+// console.log(toUpper("tatheer"));
+// console.log(toUpper("saylani"));
+
+
+//                                    TASK 03
+// Write a JavaScript function that takes an array of numbers as an argument and returns the average of the numbers.
+
+// function averageArray(arr) {
+//     let sum = 0;
+//     for(let i = 0; i < arr.length; i++) {
+//         sum = sum + arr[i];
+//     }
+//     return sum / arr.length;
+// }
+
+// console.log(averageArray([2,2]));
+// console.log(averageArray([12,12,12]));
+// console.log(averageArray([3,3,3,3]));
+
+
+//                           TASK  05
+// Write a JavaScript function that takes a string as an argument and returns the string with all letters reversed.
+
+
+// function reverseString(str) {
+//     return str.split('').reverse().join('');
+// }
+
+// console.log(reverseString("Pakistan")); 
+// console.log(reverseString("Military"));
+// console.log(reverseString("Academy")); 
+
